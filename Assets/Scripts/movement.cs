6f364b5e7fbf281e72a86e942c0d822acd776f3a
Appy-arsnel <75736454+Appy-arsnel@users.noreply.github.com>
2022-03-06 01:43:40 +0530

    day 2

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -3,67 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class movement : MonoBehaviour
-{
-    public float playerSpeed;
-    public float sprintSpeed = 4f;
-    public float walkSpeed = 2f;
-    public float mouseSensitivity = 2f;
-    public float jumpHeight = 3f;
-    private bool isMoving = false;
-    private bool isSprinting =false;
-    private float yRot;
- 
-    private Animator anim;
-    private Rigidbody rigidBody;
- 
-    // Use this for initialization
-    void Start () {
- 
-        playerSpeed = walkSpeed;
-        anim = GetComponent<Animator>();
-        rigidBody = GetComponent<Rigidbody>();
- 
+{[SerializeField]private float _speed = 7f;
+    [SerializeField]private float _mouseSensitivity = 50f;
+    [SerializeField]private float _minCameraview = -70f, _maxCameraview = 80f;
+    private CharacterController _charController;
+    private Camera _camera;
+    private float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+        _camera = Camera.main;
+
+        if(_charController == null)
+        Debug.Log("No Character Controller Attached to Player");
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
- 
+
     // Update is called once per frame
-    void Update () {
- 
-        yRot += Input.GetAxis("Mouse Y") * mouseSensitivity;
-        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, yRot, transform.localEulerAngles.z);
- 
-        isMoving = false;
- 
-        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
-        {
-            //transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * playerSpeed);
-            rigidBody.velocity += transform.right * Input.GetAxisRaw("Horizontal") * playerSpeed;
-            isMoving = true;
-        }
-        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
-        {
-            //transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * playerSpeed);
-            rigidBody.velocity += transform.forward * Input.GetAxisRaw("Vertical") * playerSpeed;
-            isMoving = true;
-        }
- 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            transform.Translate(Vector3.up * jumpHeight);
-        }
- 
-        if (Input.GetAxisRaw("Sprint") > 0f)
-        {
-            playerSpeed = sprintSpeed;
-            isSprinting = true;
-        }else if (Input.GetAxisRaw("Sprint") < 1f)
-        {
-            playerSpeed = walkSpeed;
-            isSprinting = false;
-        }
- 
-        anim.SetBool("isMoving", isMoving);
-        anim.SetBool("isSprinting", isSprinting);
- 
+    void Update()
+    {
+        //Get WASD Input for Player
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        //move player based on WASD Input
+        Vector3 movement = transform.forward * vertical + transform.right * horizontal; //changed this line.
+        _charController.Move(movement * Time.deltaTime * _speed);
+
+
+        
+        //Get Mouse position Input
+        float mouseX = Input.GetAxis("Mouse X") * _mouseSensitivity; //changed this line.
+        float mouseY = Input.GetAxis("Mouse Y") * _mouseSensitivity; //changed this line.
+          //Rotate the camera based on the Y input of the mouse
+          xRotation -= mouseY;
+          //clamp the camera rotation between 80 and -70 degrees
+          xRotation = Mathf.Clamp(xRotation, _minCameraview, _maxCameraview);
+
+          _camera.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+          //Rotate the player based on the X input of the mouse
+          transform.Rotate(Vector3.up * mouseX * 3);
+
     }
 }
- 
